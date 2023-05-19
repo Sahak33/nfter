8fea483a3d18286c05d6a18329ed2c5da2445ee7
Sahak33 <s.iskanian33@gmail.com>
2023-05-19 10:20:33 +0400

    basic connect metamask

---
+++ src/components/Wallet.tsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import Web3 from 'web3';
+
+
+const Wallet = () => {
+  const [error, setError] = useState('');
+  const [currentAccount, setCurrentAccount] = useState();
+
+  const chainChangedHandler = () => {
+    window.location.reload();
+  };
+
+  const accountChangedHandler = (newAccount: any) => {
+    setCurrentAccount(newAccount)
+  };
+
+  const handleConnect = async () => {
+    if (!window.ethereum) {
+      setError('Install metamask');
+      return;
+    }
+
+    const web3 = new Web3(window.ethereum);
+    const accounts = await web3.eth.getAccounts();
+    if (!accounts.length) {
+      console.log("Please connect to MetaMask!");
+    } else if (accounts[0] !== currentAccount) {
+      accountChangedHandler(accounts[0]);
+    }
+  };
+
+  window.ethereum.on('accountsChanged', accountChangedHandler);
+  window.ethereum.on('chainChanged', chainChangedHandler);
+
+
+  return (
+    <div>
+      <h2>address: {currentAccount}</h2>
+      <button onClick={handleConnect}>connect</button>
+    </div>
+  );
+};
+
+export default Wallet;
